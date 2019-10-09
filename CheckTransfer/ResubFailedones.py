4091e29a580670ff87ee07dd5796fc886cbce76e
junho choi <soarnsoar@gmail.com>
2019-10-09 11:15:51 +0200

    force overwrite ResubFailedones.py

--- CheckTransfer/ResubFailedones.py
+++ CheckTransfer/ResubFailedones.py
@@ -11,7 +11,7 @@ from Transfer import *
 
 f=open(INPUT_FILE)
 
-
+print "=====START JOB====="+INPUT_FILE
 #Summer16_102X_nAODv4_Full2016v5__MCl1loose2016v5__WJetsToLN__S.log
 job=latinoTransfer()
 job.Production=INPUT_FILE.split('__')[0]
@@ -22,14 +22,17 @@ lines=f.readlines()
 
 
 for s in lines:
+    s=s.replace('\n','')
     if len(s)==0 or s.replace(' ','')=="": 
         print "->empty sample name, pass"
         continue
     
     s=s.split('/')[-1]
     this_list=job.MakeList(Sample=s)
-    job.Run(this_list)
+    job.Run(this_list,True)
 
 
 from SendMail import *
 SendMail(GetScriptName())
+
+print "=====JOB FINISHED====="
